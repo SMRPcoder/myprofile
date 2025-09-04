6a4d0cef4b898986445ab5478b1696f1981c640a
Raja <102196806+SMRPcoder@users.noreply.github.com>
2025-09-04 09:00:26 +0530

    site updated

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://myprofile-lovat-tau.vercel.app/sitemap.xml',
+    sitemap: 'https://rajapandeeswaran.vercel.app/sitemap.xml',
   }
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,37 +3,37 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://myprofile-lovat-tau.vercel.app/',
+      url: 'https://rajapandeeswaran.vercel.app/',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://myprofile-lovat-tau.vercel.app/about',
+      url: 'https://rajapandeeswaran.vercel.app/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://myprofile-lovat-tau.vercel.app/skills',
+      url: 'https://rajapandeeswaran.vercel.app/skills',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
     },
     {
-        url:'https://myprofile-lovat-tau.vercel.app/myResume',
+        url:'https://rajapandeeswaran.vercel.app/myResume',
         lastModified: new Date(),
         changeFrequency:"yearly",
         priority:0.4
     },
     {
-        url:'https://myprofile-lovat-tau.vercel.app/projects',
+        url:'https://rajapandeeswaran.vercel.app/projects',
          lastModified: new Date(),
         changeFrequency:"yearly",
         priority:0.4
     },
     {
-        url:'https://myprofile-lovat-tau.vercel.app/experience',
+        url:'https://rajapandeeswaran.vercel.app/experience',
          lastModified: new Date(),
         changeFrequency:"yearly",
         priority:0.4
